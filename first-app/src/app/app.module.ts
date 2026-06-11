e5a8b78da09938c35bb365f1efccab64fc5eda03
Sumit Khandelwal <synergy2411@outlook.com>
2026-06-11 13:12:21 +0530

    users composed

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './playground/components/users/users.component';
 import { FormsModule } from '@angular/forms';
+import { UserImageComponent } from './playground/components/users/user-image/user-image.component';
+import { UserInfoComponent } from './playground/components/users/user-info/user-info.component';
 
 @NgModule({
   declarations: [
     // Component, Directives, Pipes
     AppComponent,
     UsersComponent,
+    UserImageComponent,
+    UserInfoComponent,
   ],
   imports: [
     // Modules
---
+++ first-app/src/app/playground/components/users/user-image/user-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-image',
+  templateUrl: './user-image.component.html',
+  styleUrl: './user-image.component.css'
+})
+export class UserImageComponent {
+
+}
