36c81baa08148b1c3c389eda40e2988d98176351
Sumit Khandelwal <synergy2411@outlook.com>
2026-06-11 13:26:04 +0530

    nested component communication added

--- first-app/src/app/playground/components/users/user-image/user-image.component.ts
+++ first-app/src/app/playground/components/users/user-image/user-image.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-image',
   templateUrl: './user-image.component.html',
-  styleUrl: './user-image.component.css'
+  styleUrl: './user-image.component.css',
 })
 export class UserImageComponent {
+  @Input() user: any;
 
+  @Output()
+  userEvent = new EventEmitter<any>();
+
+  onGenerate() {
+    this.userEvent.emit(this.user);
+  }
 }
