50c0a6f77c88a4eccb7b7b8b0797e7680b48ad24
Sumit Khandelwal <synergy2411@outlook.com>
2026-06-19 10:41:35 +0530

    more about signals

--- second-app/src/app/components/counter/counter.component.ts
+++ second-app/src/app/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,10 +8,24 @@ import { Component, signal } from '@angular/core';
   styleUrl: './counter.component.css',
 })
 export class CounterComponent {
-  counter = signal(0);
+  counter = signal(0); // Writable Signal
+
+  doubleCounter = computed(() => this.counter() * 2); // Readable Signals
+
+  constructor() {
+    effect(() => console.log('Value of Counter : ', this.counter()));
+  }
 
   onIncrease() {
-    // this.counter += 1;
+    // this.counter += 1;   // NEVER UPDATE SIGNAL LIKE THIS
     this.counter.update((prevValue) => prevValue + 1);
+  }
+
+  onDecrease() {
+    this.counter.update((ctr) => ctr - 1);
+  }
+
+  onReset() {
+    this.counter.set(0);
   }
 }
